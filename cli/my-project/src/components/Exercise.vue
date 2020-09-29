5f1617958acef16f01dcb319f87a74bd86cf4fc9
Malsawmpuii <princesspuii@gmail.com>
2020-09-29 15:27:06 +0630

    cli my-project

---
+++ cli/my-project/src/components/Exercise.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="testing">
+		<h2 class="text-danger">This is Exercise Component!</h2>
+	</div>
+</template>
+
+<script type="text/javascript">
+	
+
+</script> 
+
+<style type="text/css">
+	
+</style>
